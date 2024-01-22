7871b46b57520559f6e1c32f67316ddb79d3aad2
Alexander Lairich <alexanderlairich@gmx.net>
2024-01-22 16:31:32 +0100

    Applied a button to display a chatroom

--- FindFoodFriends/Pages/UserView.xaml.cs
+++ FindFoodFriends/Pages/UserView.xaml.cs
@@ -4,17 +4,19 @@ namespace FindFoodFriends.Pages;
 
 public partial class UserView : ContentView
 {
+    private readonly ScoreUser scoreuser;
+
 	public UserView(ScoreUser scoreUser)
 	{
 		InitializeComponent();
-
+        this.scoreuser = scoreUser;
         UsernameLabel.Text = scoreUser.Meta!.Name;
         ScoreLabel.Text = scoreUser.TotalMatchesPercentage;
         DetailsLabel.Text = scoreUser.TrueMatchesEntry;
     }
 
-    private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    private void Chat_Btn_Clicked(object sender, EventArgs e)
     {
-
+        Navigation.PushModalAsync(new ChatPage(scoreuser));
     }
 }
